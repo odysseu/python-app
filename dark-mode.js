56efa5bee1487c42658e6b5c9a8c72e24609579b
ulysse.boucherie <ulysse.boucherie.interieur.gouv.fr>
2024-12-27 15:13:31 +0100

    added dark icon and favicon; moved switch funciton from .css to .js

--- dark-mode.js
+++ dark-mode.js
@@ -1,22 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
     const toggleSwitch = document.getElementById("dark-mode-toggle");
     const currentTheme = localStorage.getItem("theme") ? localStorage.getItem("theme") : null;
+    const logo = document.getElementById("logo");
+    const favicon = document.getElementById("favicon");
+
+    // Function to switch logos and favicons
+    function switchTheme(theme) {
+        if (theme === "dark-mode") {
+            document.body.classList.add("dark-mode");
+            logo.src = "logo-dark.png";
+            favicon.href = "favicon-dark.ico";
+        } else {
+            document.body.classList.remove("dark-mode");
+            logo.src = "logo-light.png";
+            favicon.href = "favicon-light.ico";
+        }
+    }
 
     if (currentTheme) {
-        document.body.classList.add(currentTheme);
-
+        switchTheme(currentTheme);
         if (currentTheme === "dark-mode") {
             toggleSwitch.checked = true;
         }
     }
 
     toggleSwitch.addEventListener("change", function () {
-        if (toggleSwitch.checked) {
-            document.body.classList.add("dark-mode");
-            localStorage.setItem("theme", "dark-mode");
-        } else {
-            document.body.classList.remove("dark-mode");
-            localStorage.setItem("theme", "light-mode");
-        }
+        const theme = toggleSwitch.checked ? "dark-mode" : "light-mode";
+        switchTheme(theme);
+        localStorage.setItem("theme", theme);
     });
 });
